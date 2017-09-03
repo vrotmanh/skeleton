cff001f6e448ef331f85ed8749bf0204b7528af6
Vicente Rotman <vrotmanh@gmail.com>
2017-09-03 10:00:14 -0400

    TagController, PUT and GET methods are working

--- src/main/java/controllers/TagController.java
+++ src/main/java/controllers/TagController.java
@@ -1,9 +1,14 @@
 package controllers;
 
+import api.ReceiptResponse;
 import dao.ReceiptTagDao;
+import generated.tables.records.ReceiptsRecord;
 
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
+import java.util.List;
+
+import static java.util.stream.Collectors.toList;
 
 @Path("")
 public class TagController {
@@ -14,11 +19,17 @@ public class TagController {
     @Path("/tags/{tag}")
     @PUT
     @Consumes(MediaType.APPLICATION_JSON)
-    public void toggleTag(@PathParam("tag") String tagName) {
-        System.out.println("");
-        System.out.println("---------------TAG----------------");
-        System.out.println(tagName);
-        System.out.println("");
+    public void toggleTag(@PathParam("tag") String tagName, int reciptId) {
+        receiptTags.toggleTag(tagName, reciptId);
+    }
+
+    @Path("/tags/{tag}")
+    @GET
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public List<ReceiptResponse> getReceipts(@PathParam("tag") String tagName) {
+        List<ReceiptsRecord> receiptRecords = receiptTags.getAllReceipts(tagName);
+        return receiptRecords.stream().map(ReceiptResponse::new).collect(toList());
     }
 
 }
--- src/main/java/dao/ReceiptTagDao.java
+++ src/main/java/dao/ReceiptTagDao.java
@@ -1,11 +1,14 @@
 package dao;
 
 import generated.tables.records.ReceiptTagsRecord;
-import org.jooq.Configuration;
-import org.jooq.DSLContext;
+import generated.tables.records.ReceiptsRecord;
+import org.jooq.*;
 import org.jooq.impl.DSL;
 
+import java.util.List;
+
 import static com.google.common.base.Preconditions.checkState;
+import static generated.Tables.RECEIPTS;
 import static generated.tables.ReceiptTags.RECEIPT_TAGS;
 
 public class ReceiptTagDao {
@@ -13,6 +16,16 @@ public class ReceiptTagDao {
 
     public ReceiptTagDao(Configuration jooqConfig) {
         this.dsl = DSL.using(jooqConfig);
+    }
+
+    public int toggleTag(String tag, int receipId){
+        Result<Record> results = dsl.select().from(RECEIPT_TAGS).where(RECEIPT_TAGS.RECEIPT_ID.equal(receipId)).and(RECEIPT_TAGS.TAG.equal(tag)).fetch();
+        if(results.size() > 0){
+            dsl.delete(RECEIPT_TAGS).where(RECEIPT_TAGS.ID.in(results.getValues(RECEIPT_TAGS.ID))).execute();
+            return 0;
+        }else{
+            return insert(tag, receipId);
+        }
     }
 
     public int insert(String tag, int receiptId) {
@@ -26,4 +39,10 @@ public class ReceiptTagDao {
 
         return receiptTagRecord.getId();
     }
+
+    public List<ReceiptsRecord> getAllReceipts(String tag) {
+        Result<Record1<Integer>> receiptIds = dsl.select(RECEIPT_TAGS.RECEIPT_ID).from(RECEIPT_TAGS).where(RECEIPT_TAGS.TAG.equal(tag)).fetch();
+        return dsl.selectFrom(RECEIPTS).where(RECEIPTS.ID.in(receiptIds)).fetch();
+    }
+
 }
